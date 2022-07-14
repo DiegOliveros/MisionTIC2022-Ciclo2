580bde0461a28836881f89638159bd314d683546
DiegOliveros <diegoo.ivan@gmail.com>
2022-07-14 07:10:43 -0500

    Create usuariobibliotk.sql

---
+++ CodigosClase/base/usuariobibliotk.sql
@@ -0,0 +1,5 @@
+SELECT * FROM biblioteca.usuario;
+
+INSERT INTO `biblioteca`.`usuario` (`idusuario`, `nombre`, `password`, `biblioteca_idbiblioteca`) VALUES ('1', 'Juan', '123*', '1');
+INSERT INTO `biblioteca`.`usuario` (`idusuario`, `nombre`, `password`, `biblioteca_idbiblioteca`) VALUES ('2', 'Pedro', '123*', '2');
+INSERT INTO `biblioteca`.`usuario` (`idusuario`, `nombre`, `password`, `biblioteca_idbiblioteca`) VALUES ('3', 'Ana', '123*', '2');
